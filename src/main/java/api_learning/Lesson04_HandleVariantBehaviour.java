74b6fa578e7d36b146c7df3fa6730e2aa4ca9a48
pnghia211 <pnghia211@gmail.com>
2025-05-23 09:51:08 +0700

    leason 17 Hybrid context

--- src/main/java/api_learning/Lesson04_HandleVariantBehaviour.java
+++ src/main/java/api_learning/Lesson04_HandleVariantBehaviour.java
@@ -8,14 +8,14 @@ import io.appium.java_client.internal.CapabilityHelpers;
 import org.openqa.selenium.Capabilities;
 import platform.Lesson02_Platform;
 
-public class HandleVariantBehaviour implements Lesson02_MobileCapabilityTypeEx {
+public class Lesson04_HandleVariantBehaviour implements Lesson02_MobileCapabilityTypeEx {
     public static void main(String[] args) {
         AppiumDriver<MobileElement> appiumDriver = Lesson02_DriverFactory.getDriver(Lesson02_Platform.android);
 
         try {
             Capabilities caps = appiumDriver.getCapabilities();
             String platform = CapabilityHelpers.getCapability(caps, PLATFORM_NAME, String.class);
-            System.out.println(platform);
+            System.out.println("[INFO] Testing on: "+platform);
 
         } catch (Exception e) {
             e.printStackTrace();
